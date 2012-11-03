0827040bb94239729db7cac095308484a2ebba98
i.wind <ig-wind@mail.ru>
2012-11-04 03:27:47 +0400

    Task model class

--- db/model.py
+++ db/model.py
@@ -3,7 +3,7 @@
 """
 @script  : model.py
 @created : 2012-11-04 01:48:15.090
-@changed : 2012-11-04 03:03:45.836
+@changed : 2012-11-04 03:26:56.730
 @creator : mkpy.py --version 0.0.27
 @author  : Igor A.Vetrov <qprostu@gmail.com>
 @about   : model of TODO application
@@ -13,7 +13,7 @@ from __future__ import print_function
 from argparse import ArgumentParser
 
 
-__revision__ = 2
+__revision__ = 3
 __project__  = "Todo"
 
 
@@ -62,6 +62,24 @@ class Priority(Table):
         self.db.execSql( "insert into {} (code, name) values(?, ?)".format(self.name), (1, "Low") )
         self.db.execSql( "insert into {} (code, name) values(?, ?)".format(self.name), (2, "Medium") )
         self.db.execSql( "insert into {} (code, name) values(?, ?)".format(self.name), (3, "High") )
+        self.db.commit()
+
+
+
+class Task(Table):
+    """Task model class"""
+
+    createSql = "create table TodoTask(" \
+                "id integer primary key autoincrement not null, " \
+                "name text not null, " \
+                "priority integer references TodoPriority(code) default 2, " \
+                "deadline date not null default (date('now', 'localtime')), " \
+                "completed timestamp, " \
+                "created timestamp default (datetime('now', 'localtime')));"
+
+    def __init__(self, db):
+        self.name = __project__ + self.__class__.__name__
+        super(Task, self).__init__(db)
 
 
 
