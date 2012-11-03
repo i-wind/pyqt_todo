30f0caf111faedd67d8774b479d990a948832929
i.wind <ig-wind@mail.ru>
2012-11-04 03:29:25 +0400

    Testing Task model class

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@
 """
 @script  : test_model.py
 @created : 2012-11-04 02:28:46.742
-@changed : 2012-11-04 02:28:46.742
+@changed : 2012-11-04 03:28:35.538
 @creator : mkpy.py --version 0.0.27
 @author  : Igor A.Vetrov <qprostu@gmail.com>
 @about   : testing application model classes
@@ -11,26 +11,16 @@
 import os, sys
 sys.path.insert( 0, os.path.normpath( os.path.join( os.getcwd(), '..' ) ) )
 from db.sqlite import SQLite
-from db.model import Priority
+from db.model import Priority, Task
 import threading
 import unittest
 
 
-__revision__ = 1
+__revision__ = 2
 
 
 
 class PriorityTable(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
 
     def setUp(self):
         with threading.Lock():
@@ -63,6 +53,29 @@ class PriorityTable(unittest.TestCase):
     def test_high(self):
         row = self.db.execSql( "select name from {} where code=?;".format(self.table.name), (3,) )[0]
         self.assertEqual( row["name"], "High" )
+
+
+
+class TaskTable(unittest.TestCase):
+
+    def setUp(self):
+        with threading.Lock():
+            self.dbName = "test.sqlite3"
+            self.db = SQLite(self.dbName)
+            self.priority = Priority(self.db)
+            self.table = Task(self.db)
+
+
+    def tearDown(self):
+        with threading.Lock():
+            if os.path.exists(self.dbName):
+                self.db.__del__()
+                os.unlink(self.dbName)
+
+
+    def test_table_exists(self):
+        self.assertTrue( self.db.tableExists(self.priority.name) )
+        self.assertTrue( self.db.tableExists(self.table.name) )
 
 
 
